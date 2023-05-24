084271386dd2c9b1e7675139fea06719d8f03a80
loicbaecher <74916339+loicbaecher@users.noreplay.github.com>
2023-05-24 11:30:51 +0200

    add form control, add read/unread button

--- library.js
+++ library.js
@@ -8,17 +8,19 @@ function Book(title,author,pages,read) {
     this.info = function(){
         return(`${title} by ${author}, ${pages} pages, ${read}`);
     }
+    this.readstat = function() {
+        if (this.read == "Read"){
+            this.read = "Not read yet";
+        }
+        else{
+            this.read ="Read";
+        }
+    }
 }
-
-let Book1 = new Book ('The Hobbit','J.R.R Tolkien','295','not read yet')
-let Book2 = new Book ('The Bible','Several authors','2151','not read yet')
 
 function addBookToLibrary(book){
        myLibrary.push(book);
 }
-
-addBookToLibrary(Book1)
-addBookToLibrary(Book2)
 
 function displayBooks(){
     booklist = document.getElementById("booklist");
@@ -38,8 +40,9 @@ function displayBooks(){
         newbook.setAttribute("class","newbook");
         newbook.setAttribute("id",`book${i+1}`);
         newbook.setAttribute("data",`${i+1}`);
-        newbook.setAttribute("style","border: 4px solid black; padding:4px;")
-
+        newbook.setAttribute("style","border: 4px solid black; padding:4px;");
+        newbook.style.display = "flex";
+        newbook.style.flexDirection = "column";
         Bookdiv.appendChild(newbook);
 
         curul = document.createElement("ul");
@@ -49,41 +52,69 @@ function displayBooks(){
         let li1 = document.createElement("li");
         let li2 = document.createElement("li");
         let li3 = document.createElement("li");
-        let li4 = document.createElement("li");
         li1.textContent = `Title : ${myLibrary[i]["title"]}`;
         li2.textContent = `Author : ${myLibrary[i]["author"]}`;
-        li3.textContent = `Pages : ${myLibrary[i]["pages"]}`;
-        li4.textContent = `Read : ${myLibrary[i]["read"]}`;
+        li3.textContent = `${myLibrary[i]["pages"]} pages`;
         curul.appendChild(li1);
         curul.appendChild(li2);
         curul.appendChild(li3);
-        curul.appendChild(li4);
+
+        underblock = document.createElement("div");
+        newbook.appendChild(underblock);
+        underblock.style.display = "flex";
+        underblock.style.justifyContent = "space-evenly"
+
+        readdiv = document.createElement("button");
+        readdiv.textContent = `${myLibrary[i]["read"]}`;
+        if(myLibrary[i]["read"]=="Read"){
+            readdiv.style.backgroundColor = "#98D8AA";
+        }
+        else {
+            readdiv.style.backgroundColor = "#FF6D60";
+        }
+        readdiv.style.border = "1px solid gray";
+        readdiv.setAttribute("id",`readdiv${i+1}`);
+        readdiv.setAttribute("class","readdiv");
+        readdiv.style.width = "fit-content";
+        readdiv.style.borderRadius = "5px";
+        underblock.appendChild(readdiv);
 
         let delbook = document.createElement("button");
         delbook.textContent = "X";
         delbook.style.backgroundColor = "#73A9AD";
         delbook.style.border = "1px solid gray";
         delbook.style.borderRadius = "5px";
+        delbook.style.display = "flex";
+        delbook.style.justifySelf = "right";
         delbook.setAttribute("id",`remobook${i+1}`);
         delbook.setAttribute("class",`delobooks`);
-        newbook.appendChild(delbook);
+        underblock.appendChild(delbook);
     }
 
     delbot = document.querySelectorAll("button.delobooks")
     for (let j = 0; j < delbot.length; j++) {
         delbot[j].addEventListener('click', function() {
             kickbook = document.querySelector(`.newbook[data="${j+1}"]`)
-            console.log(j+1);
-            console.log(kickbook);
             kickbook.remove();
             myLibrary.splice(j,1)
         });
       }
-}
 
-function remobook(){
-    var remo = document.querySelectorAll('button.delobooks')
-    
+      readbot = document.querySelectorAll("button.readdiv")
+    for (let j = 0; j < readbot.length; j++) {
+        readbot[j].addEventListener('click', function() {
+            myLibrary[j].readstat();
+            selbook = document.querySelector(`#readdiv${j+1}`);
+            if(myLibrary[j]["read"] == "Read"){
+                selbook.style.backgroundColor = "#98D8AA";
+                selbook.textContent = "Read";
+            }
+            else{ 
+                selbook.style.backgroundColor = "#FF6D60";
+                selbook.textContent = "Not read yet";
+        }
+        });
+      }
 }
 
 function openModal(){
@@ -117,28 +148,31 @@ closeb.addEventListener('click',()=>{
 var modsub = document.getElementById("modub");
 
 modsub.addEventListener('click',(event)=>{
-    event.preventDefault();
     gettitle = document.getElementById("fortitle");
     getauthor = document.getElementById("forauthor");
     getforpages = document.getElementById("forpages");
 
-    function getSelectedValue() {
-        var radios = document.getElementsByName('read');
-      
-        for (var i = 0; i < radios.length; i++) {
-          if (radios[i].checked) {
-            var selectedValue = radios[i].value;
-            console.log(selectedValue);
-            return(selectedValue);
-            break;
-          }
-        }
-      }
+    if(gettitle.value != "" && getauthor.value != "" && getforpages.value != ""){
+
+        event.preventDefault();
+        function getSelectedValue() {
+            var radios = document.getElementsByName('read');
+            
+            for (var i = 0; i < radios.length; i++) {
+                if (radios[i].checked) {
+                var selectedValue = radios[i].value;
+                return(selectedValue);
+                break;
+                }
+            }
+            }
 
 
-    newbook = new Book(gettitle.value,getauthor.value,getforpages.value,getSelectedValue())
-    addBookToLibrary(newbook);
-    closeModal();
+        newbook = new Book(gettitle.value,getauthor.value,getforpages.value,getSelectedValue())
+        addBookToLibrary(newbook);
+        closeModal();
+
+    }
 
 },false)
 
